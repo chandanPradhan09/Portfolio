cf9069d3c5a6aa6391a4ad1c019525967fe31fa9
Chandan <chandan09112001@gmail.com>
2023-06-26 00:24:57 +0530

    Added Drive links

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
+import Typed from "typed.js";
 
-import Typed from 'typed.js';
-
-const typed = new Typed('#element', {
-  strings: ["Programmer.", "Developer.", "Engineer.", "Student."],
-  typeSpeed: 100,
-  loop: true,
-  loopCount: Infinity,
-  showCursor: true,
-  cursorChar: '_',
-  backDelay: 700,
+const typed = new Typed("#element", {
+	strings: ["Programmer.", "Developer.", "Engineer.", "Student."],
+	typeSpeed: 100,
+	loop: true,
+	loopCount: Infinity,
+	showCursor: true,
+	cursorChar: "_",
+	backDelay: 700,
 });
 
 const layer = document.getElementById("layer");
@@ -19,44 +18,39 @@ window.addEventListener("mousemove", (e) => {
 	);
 });
 
-
 // Get all the navigation links
-const navLinks = document.querySelectorAll('nav ul li a');
+const navLinks = document.querySelectorAll("nav ul li a");
 
 // Function to check if an element is fully visible at the top of the viewport
 function isFullyVisibleAtTop(element) {
-  const elementRect = element.getBoundingClientRect();
-  return elementRect.top >= 0 && elementRect.top <= window.innerHeight * 0.5;
+	const elementRect = element.getBoundingClientRect();
+	return elementRect.top >= 0 && elementRect.top <= window.innerHeight * 0.5;
 }
 
 // Function to handle scroll event
 function handleScroll() {
-  let activeLink = null;
+	let activeLink = null;
 
-  // Find the currently active link, if any
-  navLinks.forEach(link => {
-    const target = document.querySelector(link.hash);
+	// Find the currently active link, if any
+	navLinks.forEach((link) => {
+		const target = document.querySelector(link.hash);
 
-    if (isFullyVisibleAtTop(target)) {
-      activeLink = link;
-    }
-  });
+		if (isFullyVisibleAtTop(target)) {
+			activeLink = link;
+		}
+	});
 
-  // Add the "active" class to the appropriate navigation link
-  if (activeLink) {
-	navLinks.forEach(link=>{
-		link.classList.remove("active");
-	})
-    activeLink.classList.add('active');
-  }
+	// Add the "active" class to the appropriate navigation link
+	if (activeLink) {
+		navLinks.forEach((link) => {
+			link.classList.remove("active");
+		});
+		activeLink.classList.add("active");
+	}
 }
 
 // Attach the scroll event listener
-window.addEventListener('scroll', handleScroll);
+window.addEventListener("scroll", handleScroll);
 
 // Add the "active" class to the appropriate navigation link on page load
-window.addEventListener('load', handleScroll);
-
-
-
-
+window.addEventListener("load", handleScroll);
